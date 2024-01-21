e4db0a8f3e01d0ab479629387ca711b8ea5b441c
Turi Souza <turionline@gmail.com>
2024-01-21 19:14:04 -0300

    Correção de bug

--- js/camera.js
+++ js/camera.js
@@ -36,5 +36,5 @@ botaoEnviarFoto.addEventListener("click", () => {
 
     localStorage.setItem("cadastro", JSON.stringify(converteRetorno))
 
-    window.location.href = "./abrir-conta-form -3.html"
+    window.location.href = "./abrir-conta-form-3.html"
 })
